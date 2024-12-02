58666c1e226e6596f5fc96b3adff3e9289beb2d0
LeonardoR998 <leonardoricci839@gmail.com>
2024-12-02 14:32:35 +0100

    add comments in btn.jsx

--- src/components/ButtonList.jsx
+++ src/components/ButtonList.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 
+// Componente ButtonList
+// Props:
+// - onSelect: una funzione callback per gestire il linguaggio selezionato
 const ButtonList = ({ languages, onSelect }) => {
   return (
     <div className="mb-3">
+      {/* Mappa l'array `languages` per creare un pulsante per ogni linguaggio */}
       {languages.map((language) => (
         <button
-          key={language.id}
-          onClick={() => onSelect(language)}
+          key={language.id} // Chiave per ogni pulsante
+          onClick={() => onSelect(language)} // Chiama la funzione `onSelect` passando il linguaggio selezionato
           className="btn btn-outline-primary me-2"
         >
-          {language.title}
+          {language.title}{" "}
+          {/* Mostra il titolo del linguaggio come testo del pulsante */}
         </button>
       ))}
     </div>
